cb912a15fd7fb2ac2c98b9decd2f9af0622b56dd
Patrick Kleinschmidt <noxifoxi@gmail.com>
2015-12-16 23:46:35 +0100

    Fixed author line

--- src/config/ConfigNodeNotFoundException.php
+++ src/config/ConfigNodeNotFoundException.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @author    aelix framework <info@aelix framework.org>
+ * @author    aelix framework <info@aelix.org>
  * @copyright Copyright (c) 2015 aelix framework
  * @license   http://opensource.org/licenses/gpl-3.0.html GNU General Public License, version 3
  */
